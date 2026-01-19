131945bfe961db875d2cdeba1b457c05c864e8f6
Harry Chopra <22281143+harrychopra@users.noreply.github.com>
2026-01-19 01:00:00 +0000

    initial commit

---
+++ challenges/1-cat-server.js
@@ -0,0 +1,34 @@
+const server = require('../utils/server');
+// Do not change anything above this line
+
+function checkServerStatus() {}
+
+function fetchBannerContent() {}
+
+function fetchAllOwners() {}
+
+function fetchCatsByOwner() {}
+
+function fetchCatPics() {}
+
+function fetchAllCats() {}
+
+function fetchOwnersWithCats() {}
+
+function kickLegacyServerUntilItWorks() {}
+
+function buySingleOutfit() {}
+
+// Do not change anything below this line
+module.exports = {
+  buySingleOutfit,
+  checkServerStatus,
+  kickLegacyServerUntilItWorks,
+  fetchAllCats,
+  fetchCatPics,
+  fetchAllOwners,
+  fetchBannerContent,
+  fetchOwnersWithCats,
+  fetchCatsByOwner,
+  server
+};
